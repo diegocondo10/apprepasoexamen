4201ecedefbd7d242c1349b3a41516f075e05fc3
MrRainx <diegocondo10@yahoo.es>
2019-07-29 22:43:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/app_repaso_examen/models/Cliente.java
@@ -0,0 +1,97 @@
+package com.example.app_repaso_examen.models;
+
+public class Cliente {
+
+    private int id;
+    private String identificacion;
+    private String nombres;
+    private String apellidos;
+    private String correo;
+    private String sexo;
+    private String estadoCivil;
+
+
+    public Cliente(int id, String identificacion, String nombres, String apellidos, String correo, String sexo, String estadoCivil) {
+        this.id = id;
+        this.identificacion = identificacion;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.sexo = sexo;
+        this.estadoCivil = estadoCivil;
+    }
+
+    public Cliente() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", identificacion='" + identificacion + '\'' +
+                ", nombres='" + nombres + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", correo='" + correo + '\'' +
+                ", sexo='" + sexo + '\'' +
+                ", estadoCivil='" + estadoCivil + '\'' +
+                '}';
+    }
+
+
+}
